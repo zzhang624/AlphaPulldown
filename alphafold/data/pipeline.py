a25af4ddcf226d9ccc5b2c9c6ace28a18edf7925
Geoffrey Yu <geoffrey.yu96@icloud.com>
2023-04-17 15:19:23 +0200

    remove extra_msa_db

--- alphafold/data/pipeline.py
+++ alphafold/data/pipeline.py
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 """Functions for building the input features for the AlphaFold model."""
+
 
 import os
 from typing import Any, Mapping, MutableMapping, Optional, Sequence, Union
@@ -27,10 +29,14 @@ from alphafold.data.tools import hmmsearch
 from alphafold.data.tools import jackhmmer
 import numpy as np
 
+
 # Internal import (7716).
+
 
 FeatureDict = MutableMapping[str, np.ndarray]
 TemplateSearcher = Union[hhsearch.HHSearch, hmmsearch.Hmmsearch]
+
+
 
 
 def make_sequence_features(
@@ -50,10 +56,13 @@ def make_sequence_features(
   return features
 
 
+
+
 def make_msa_features(msas: Sequence[parsers.Msa]) -> FeatureDict:
   """Constructs a feature dict of MSA features."""
   if not msas:
     raise ValueError('At least one MSA must be provided.')
+
 
   int_msa = []
   deletion_matrix = []
@@ -71,8 +80,8 @@ def make_msa_features(msas: Sequence[parsers.Msa]) -> FeatureDict:
       deletion_matrix.append(msa.deletion_matrix[sequence_index])
       identifiers = msa_identifiers.get_identifiers(
           msa.descriptions[sequence_index])
-      print(f"pipeline.py line 74 species_id is :{identifiers.species_id.encode('utf-8')}\n description is : {msa.descriptions[sequence_index]}")
       species_ids.append(identifiers.species_id.encode('utf-8'))
+
 
   num_res = len(msas[0].sequences[0])
   num_alignments = len(int_msa)
@@ -83,6 +92,8 @@ def make_msa_features(msas: Sequence[parsers.Msa]) -> FeatureDict:
       [num_alignments] * num_res, dtype=np.int32)
   features['msa_species_identifiers'] = np.array(species_ids, dtype=np.object_)
   return features
+
+
 
 
 def run_msa_tool(msa_runner, input_fasta_path: str, msa_out_path: str,
@@ -109,8 +120,11 @@ def run_msa_tool(msa_runner, input_fasta_path: str, msa_out_path: str,
   return result
 
 
+
+
 class DataPipeline:
   """Runs the alignment tools and assembles the input features."""
+
 
   def __init__(self,
                jackhmmer_binary_path: str,
@@ -126,7 +140,7 @@ class DataPipeline:
                mgnify_max_hits: int = 501,
                uniref_max_hits: int = 10000,
                use_precomputed_msas: bool = False):
- 
+    """Initializes the data pipeline."""
     self._use_small_bfd = use_small_bfd
     self.jackhmmer_uniref90_runner = jackhmmer.Jackhmmer(
         binary_path=jackhmmer_binary_path,
@@ -161,6 +175,7 @@ class DataPipeline:
     input_description = input_descs[0]
     num_res = len(input_sequence)
 
+
     uniref90_out_path = os.path.join(msa_output_dir, 'uniref90_hits.sto')
     jackhmmer_uniref90_result = run_msa_tool(
         msa_runner=self.jackhmmer_uniref90_runner,
@@ -178,10 +193,12 @@ class DataPipeline:
         use_precomputed_msas=self.use_precomputed_msas,
         max_sto_sequences=self.mgnify_max_hits)
 
+
     msa_for_templates = jackhmmer_uniref90_result['sto']
     msa_for_templates = parsers.deduplicate_stockholm_msa(msa_for_templates)
     msa_for_templates = parsers.remove_empty_columns_from_stockholm_msa(
         msa_for_templates)
+
 
     if self.template_searcher.input_format == 'sto':
       pdb_templates_result = self.template_searcher.query(msa_for_templates)
@@ -192,22 +209,20 @@ class DataPipeline:
       raise ValueError('Unrecognized template input format: '
                        f'{self.template_searcher.input_format}')
 
+
     pdb_hits_out_path = os.path.join(
         msa_output_dir, f'pdb_hits.{self.template_searcher.output_format}')
     with open(pdb_hits_out_path, 'w') as f:
       f.write(pdb_templates_result)
 
-    jackhmmer_uniref90_result['a3m'] = parsers.convert_stockholm_to_a3m(jackhmmer_uniref90_result['sto'])
-    jackhmmer_mgnify_result['a3m'] = parsers.convert_stockholm_to_a3m(jackhmmer_mgnify_result['sto'])
-    # uniref90_msa = parsers.parse_stockholm(jackhmmer_uniref90_result['sto'])
-    print("pipeline.py Now process jackhmmer_mgnify_result")
+
+    uniref90_msa = parsers.parse_stockholm(jackhmmer_uniref90_result['sto'])
     mgnify_msa = parsers.parse_stockholm(jackhmmer_mgnify_result['sto'])
-    # extra_msa = parsers.parse_stockholm(extra_msa_result['sto'])
-    print(f"pipeline.py Now process jackhmmer_uniref90_result")
-    uniref90_msa = parsers.parse_a3m(jackhmmer_uniref90_result['a3m'])
-    # mgnify_msa = parsers.parse_a3m(jackhmmer_mgnify_result['a3m'])
+
+
     pdb_template_hits = self.template_searcher.get_template_hits(
         output_string=pdb_templates_result, input_sequence=input_sequence)
+
 
     if self._use_small_bfd:
       bfd_out_path = os.path.join(msa_output_dir, 'small_bfd_hits.sto')
@@ -228,16 +243,20 @@ class DataPipeline:
           use_precomputed_msas=self.use_precomputed_msas)
       bfd_msa = parsers.parse_a3m(hhblits_bfd_uniref_result['a3m'])
 
+
     templates_result = self.template_featurizer.get_templates(
         query_sequence=input_sequence,
         hits=pdb_template_hits)
+
 
     sequence_features = make_sequence_features(
         sequence=input_sequence,
         description=input_description,
         num_res=num_res)
 
+
     msa_features = make_msa_features((uniref90_msa, bfd_msa, mgnify_msa))
+
 
     logging.info('Uniref90 MSA size: %d sequences.', len(uniref90_msa))
     logging.info('BFD MSA size: %d sequences.', len(bfd_msa))
@@ -247,5 +266,6 @@ class DataPipeline:
     logging.info('Total number of templates (NB: this can include bad '
                  'templates and is later filtered to top 4): %d.',
                  templates_result.features['template_domain_names'].shape[0])
+
 
     return {**sequence_features, **msa_features, **templates_result.features}
